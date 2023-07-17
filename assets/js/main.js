6ed3f26d58bcc02d3d1ba32b633992794cbacef8
Alayande olaoluwa Stephen <alayandesteven@gmail.com>
2023-07-17 19:15:29 +0100

    mush

---
+++ assets/js/main.js
@@ -0,0 +1,47 @@
+const dropdownItems = document.querySelectorAll('.nav-list-hover')
+
+dropdownItems.forEach(dropdownItem => {
+    dropdownItem.addEventListener("mouseover",() => {
+        dropdownItem.lastElementChild.style.cssText = `opacity:.9; visibility:visible`
+        document.querySelector('.navbar-wrapper').style.background = `linear-gradient(to right, #066399, #2f8fdf,#066399)`
+    })
+
+    dropdownItem.addEventListener("mouseout",() => {
+        dropdownItem.lastElementChild.style.cssText = `opacity:0; visibility:hidden`
+        document.querySelector('.navbar-wrapper').style.background = `none`
+    })
+})
+
+
+// *********** slide background *****************//
+
+function slideBackground() {
+    for (let i = 0; i <= 3; i++) {
+        const div  = document.createElement('div');
+        div.style.backgroundImage = `url(images/landing-${i}.jpg)`;
+
+        i === 1 && div.classList.add('change');
+        document.querySelector('slideBackground').appendChild('div');
+    }
+}
+
+slideBackground();
+
+const divs = document.querySelectorAll('.slideBackground div')
+let a = 1
+const slideshow = ()=> {
+    setInterval(()=> {
+        a++
+        const div  = document.querySelector('.slideBackground .change');
+        div.classList.remove('change');
+
+        if (a < divs.length) {
+            div.nextElementSibling.classList.add('change')
+        }else {
+            divs[0].classList.add('change')
+            a = 1
+        }
+    }, 2000)
+}
+
+slideshow();
